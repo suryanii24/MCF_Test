2d50025bb094a48767908b7b74e0fc3b0eebc6b0
suryanii24 <suryanirachma@gmail.com>
2024-08-09 14:17:08 +0700

    Add project files.

---
+++ MCF_Test/Models/TrBpkb.cs
@@ -0,0 +1,19 @@
+﻿namespace MCF_Test.Models
+{
+    public class TrBpkb
+    {
+        public string AgreementNumber { get; set; }
+        public string BpkbNo { get; set; }
+        public string BranchId { get; set; }
+        public DateTime BpkbDate { get; set; }
+        public string FakturNo { get; set; }
+        public DateTime FakturDate { get; set; }
+        public string LocationId { get; set; }
+        public string PoliceNo { get; set; }
+        public DateTime BpkbDateIn { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public DateTime LastUpdatedOn { get; set; }
+    }
+}
